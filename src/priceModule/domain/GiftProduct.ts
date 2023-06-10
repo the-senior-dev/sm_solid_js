6f023de7bac677052c571657b7c52bc09908eef3
Bogdan Nedelcu <bogdanned32@gmail.com>
2023-06-10 12:23:54 +0200

    feat: gift product

--- src/priceModule/domain/GiftProduct.ts
+++ src/priceModule/domain/GiftProduct.ts
@@ -1,9 +1,16 @@
 import Product from "./Product";
 
 export default class GiftProduct extends Product {
-  private isTaxable = true;
+  private isTaxable = false;
+
   calculateTotalPriceWithTax(taxRate: number): number {
-    // violation of LSP
-    throw new Error("Gift products are not taxable");
+    // Behaves like the parent class ✅✅✅
+    // Rather than throw an error, just ignore the tax for gift products
+    if (this.isTaxable) {
+      return super.calculateTotalPriceWithTax(taxRate);
+    } else {
+      // If the product is not taxable, return the total price without tax
+      return this.calculateTotalPrice(); // 🎉 Test Passed! 🎉
+    }
   }
 }
